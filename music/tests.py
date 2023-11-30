00b143c305f2dd43d0abcf86e449bcbb884a5c4a
helled-hell <83688118+helled-hell@users.noreply.github.com>
2023-11-30 19:18:20 +0600

    Update tests.py

--- music/tests.py
+++ music/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .models import Spotik
 
-# Create your tests here.
+class MusicTestCase(TestCase):
+    def test_model_name(self):
+        spotify = Spotik.objects.create(name="song", artist="author")
+        self.assertEqual(spotify.name, "song")
+
+    def test_model_url(self):
+        spotify = Spotik.objects.create(name="song", artist="author")
+        self.assertEqual(spotify.artist, "author")
+      # Create your tests here.
